37cdd5358e39d1539f226743fe96324ec87c480e
Argo Roots <argo@roots.ee>
2015-11-25 03:02:37 +0200

    Refactored to use Bootstrap

--- gruntfile.js
+++ gruntfile.js
@@ -4,14 +4,8 @@ module.exports = function (grunt) {
             build: {
                 src: 'tmp'
             },
-        },
-        copy: {
-            fonts: {
-                cwd: 'bower_components/ionic/release/',
-                src: ['fonts/*.*'],
-                dest: '',
-                expand: true,
-                // flatten: true,
+            postbuild: {
+                src: ['tmp']
             }
         },
         jade: {
@@ -21,7 +15,7 @@ module.exports = function (grunt) {
                 },
                 files: [{
                     expand: true,
-                    cwd: 'source/views',
+                    cwd: 'views',
                     src: ['index.jade'],
                     dest: 'tmp',
                     ext: '.html'
@@ -31,52 +25,31 @@ module.exports = function (grunt) {
         stylus: {
             css: {
                 files: {
-                    'tmp/application.css': [
-                        'source/stylesheets/*.styl'
-                    ]
+                    'tmp/application.css': ['stylesheets/*.styl']
                 }
             }
         },
-        replace : {
-            frameworks: {
-                replacements: [{
-                    from: '../fonts/',
-                    to: './fonts/'
-                }],
-                src: ['bower_components/ionic/release/css/ionic.css'],
-                dest: 'tmp/ionic.css'
-            }
-        },
         cssmin : {
-            frameworks: {
+            application: {
                 options: {
                     keepSpecialComments: 0,
                 },
                 files: {
-                    'tmp/stylesheet.css': [
-                        'tmp/ionic.css',
-                        'tmp/application.css'
-                    ]
+                    'tmp/stylesheet.css': ['tmp/application.css']
                 }
             }
         },
         uglify: {
-            frameworks: {
+            application: {
                 files: {
-                    'tmp/javascript.js': [
-                        'bower_components/ionic/release/js/ionic.bundle.js',
-                        'bower_components/angular-resource/angular-resource.js',
-                        'source/javascripts/*.js'
-                    ]
+                    'tmp/javascript.js': ['bower_components/async/dist/async.js', 'javascripts/*.js']
                 }
             }
         },
         includereplace: {
             all: {
                 files: {
-                    'index.html': [
-                        'tmp/index.html'
-                    ]
+                    'index.html': ['tmp/index.html']
                 }
             }
         },
@@ -84,22 +57,22 @@ module.exports = function (grunt) {
             server: {
                 options: {
                     port: 4000,
-                    base: './',
+                    base: '',
                     hostname: 'localhost'
                 }
             }
         },
         watch: {
             stylesheets: {
-                files: 'source/**/*.styl',
-                tasks: ['stylus', 'cssmin', 'includereplace']
+                files: '**/*.styl',
+                tasks: ['stylus', 'cssmin:application', 'jade', 'includereplace']
             },
             scripts: {
-                files: 'source/**/*.js',
-                tasks: ['uglify', 'includereplace']
+                files: '**/*.js',
+                tasks: ['uglify:application', 'jade', 'includereplace']
             },
             jade: {
-                files: 'source/**/*.jade',
+                files: '**/*.jade',
                 tasks: ['jade', 'includereplace']
             },
         }
@@ -107,25 +80,28 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean')
     grunt.loadNpmTasks('grunt-contrib-connect')
-    grunt.loadNpmTasks('grunt-contrib-copy')
     grunt.loadNpmTasks('grunt-contrib-cssmin')
     grunt.loadNpmTasks('grunt-contrib-jade')
-    grunt.loadNpmTasks('grunt-contrib-less')
     grunt.loadNpmTasks('grunt-contrib-stylus')
     grunt.loadNpmTasks('grunt-contrib-uglify')
     grunt.loadNpmTasks('grunt-contrib-watch')
     grunt.loadNpmTasks('grunt-include-replace')
-    grunt.loadNpmTasks('grunt-text-replace')
+
+    grunt.registerTask(
+        'prepare',
+        'Compiles all of the assets and copies the files to the build directory.',
+        ['clean:build', 'jade', 'stylus', 'cssmin', 'uglify', 'includereplace']
+    )
 
     grunt.registerTask(
         'build',
         'Compiles all of the assets and copies the files to the build directory. Cleanup all mess.',
-        ['clean', 'copy', 'jade', 'stylus', 'replace', 'cssmin', 'uglify', 'includereplace']
+        ['prepare', 'clean:postbuild']
     )
 
     grunt.registerTask(
         'default',
         'Watches the project for changes, automatically builds them and runs a server.',
-        ['build', 'connect', 'watch']
+        ['prepare', 'connect', 'watch']
     )
 }
---
+++ javascripts/script.js
@@ -0,0 +1,253 @@
+var entuAPI = 'https://nommelumepark.entu.ee/api2/'
+var entuAPI3 = 'https://auth.entu.ee/'
+var erplyAPI = 'https://nommelumepark.entu.ee/erply/'
+
+var cl = function(data) {
+    console.log(data)
+}
+
+
+
+// ENTU HELPERS
+var entu = {}
+
+entu.getUser = function(user_id, user_token, http, callback) {
+    http.get(entuAPI + 'user', {
+            headers: {
+                'X-Auth-UserId': user_id,
+                'X-Auth-Token': user_token
+            }
+        })
+        .success(function(data) {
+            if(data.result) {
+                var user = {
+                    id: data.result.id,
+                    token: data.result.session_key,
+                    name: data.result.name
+                }
+
+                callback(null, user)
+            } else {
+                callback(new Error('No user'))
+            }
+        })
+        .error(function(error) {
+            cl(error)
+            callback(new Error('No user'))
+        })
+}
+
+entu.getEntities = function(params, user_id, user_token, http, callback) {
+    http.get(entuAPI + 'entity', {
+            headers: {
+                'X-Auth-UserId': user_id,
+                'X-Auth-Token': user_token
+            },
+            params: params
+        })
+        .success(function(data) {
+            if(data.result !== []) {
+                callback(null, data.result)
+            } else {
+                callback(new Error('No entities'))
+            }
+        })
+        .error(function(error) {
+            cl(error)
+            callback(new Error('No entities'))
+        })
+}
+
+
+entu.getEntity = function(entity_id, user_id, user_token, http, callback) {
+    http.get(entuAPI + 'entity-' + entity_id, {
+            headers: {
+                'X-Auth-UserId': user_id,
+                'X-Auth-Token': user_token
+            }
+        })
+        .success(function(data) {
+            if(data.result) {
+                var entity = {
+                    id: data.result.id,
+                    name: data.result.displayname,
+                    info: data.result.displayinfo,
+                    properties: data.result.properties
+                }
+
+                callback(null, entity)
+            } else {
+                callback(new Error('No entity #' + entity_id + ''))
+            }
+        })
+        .error(function(error) {
+            cl(error)
+            callback(new Error('No entity #' + entity_id + ''))
+        })
+}
+
+
+
+
+
+angular.module('lumeparkApp', ['ngRoute'])
+
+
+
+//ROUTER
+    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+        $locationProvider.html5Mode(false)
+        $routeProvider
+            .when('/', {
+                templateUrl: 'start',
+                controller: 'startCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'start',
+                controller: 'loginCtrl'
+            })
+            .when('/auth', {
+                templateUrl: 'start',
+                controller: 'authCtrl'
+            })
+            .when('/logout', {
+                templateUrl: 'start',
+                controller: 'logoutCtrl'
+            })
+            .when('/lendings/:filter', {
+                templateUrl: 'lendings',
+                controller: 'lendingsCtrl'
+            })
+            .when('/lending/:id', {
+                templateUrl: 'lending',
+                controller: 'lendingCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            })
+    }])
+
+
+
+// ANALYTICS
+    .run(['$rootScope', '$location', function($rootScope, $location) {
+        // $rootScope.$on('$routeChangeSuccess', function(){
+        //     ga('send', 'pageview', {page: $location.path(), title: $location.path().substring(1).replace('/', ' - ')})
+        // })
+    }])
+
+
+
+// START
+    .controller('startCtrl', ['$rootScope', '$http', '$window', function($rootScope, $http, $window) {
+        $rootScope.entu_url = entuAPI
+        $rootScope.listFilter = null
+
+        entu.getUser($window.sessionStorage.getItem('user_id'), $window.sessionStorage.getItem('user_token'), $http, function(error, user) {
+            if(error) {
+                $rootScope.user = null
+            } else {
+                $rootScope.user = user
+            }
+        })
+
+    }])
+
+
+
+// LOGIN
+    .controller('loginCtrl', ['$http', '$location', '$window', function($http, $location, $window){
+        var state = '1234567890abcdef'
+
+        $window.sessionStorage.clear()
+        $window.sessionStorage.setItem('state', state)
+
+        $http.post(entuAPI + 'user/auth', {state: state, redirect_url: $location.protocol() + '://' + location.host + '/#/auth'})
+            .success(function(data) {
+                $window.sessionStorage.setItem('auth_url', data.result.auth_url)
+                $window.location.href = data.result.auth_url
+            })
+            .error(function(data) {
+                cl(data)
+            })
+    }])
+
+
+
+// AUTH AFTER LOGIN
+    .controller('authCtrl', ['$http', '$window', function($http, $window){
+        var auth_url = $window.sessionStorage.getItem('auth_url')
+        var state = $window.sessionStorage.getItem('state')
+
+        $http.post(auth_url, {state: state})
+            .success(function(data) {
+                $window.sessionStorage.clear()
+                $window.sessionStorage.setItem('user_id', data.result.user.id)
+                $window.sessionStorage.setItem('user_token', data.result.user.session_key)
+                $window.location.href = '/'
+            })
+            .error(function(data) {
+                cl(data)
+            })
+    }])
+
+
+
+// LOGOUT
+    .controller('logoutCtrl', ['$rootScope', '$location', '$window', function($rootScope, $location, $window){
+        $window.sessionStorage.clear()
+        $rootScope.user = null
+        $window.location.href = entuAPI3 + 'exit?next=' + $location.protocol() + '://' + location.host
+    }])
+
+
+
+// LENDINGS
+    .controller('lendingsCtrl', ['$scope', '$rootScope', '$http', '$routeParams', '$window', function($scope, $rootScope, $http, $routeParams, $window){
+        $rootScope.listFilter = $routeParams.filter
+
+        $scope.loading = true
+
+        async.waterfall([
+            function getUser(callback) {
+                entu.getUser($window.sessionStorage.getItem('user_id'), $window.sessionStorage.getItem('user_token'), $http, function(error, user) {
+                    if(error) {
+                        callback(error)
+                    } else {
+                        $rootScope.user = user
+
+                        callback(null, user)
+                    }
+                })
+            },
+            function getLendings(user, callback) {
+                entu.getEntities({ definition: 'laenutus' }, $rootScope.user.id, $rootScope.user.token, $http, function(error, lendings) {
+                    if(error) {
+                        callback(error)
+                    } else {
+                        callback(null, lendings)
+                    }
+                })
+            },
+            function getEachLending(lendings, callback) {
+                $scope.lendings = []
+                async.each(lendings, function(lending) {
+                    entu.getEntity(lending.id, $rootScope.user.id, $rootScope.user.token, $http, function(error, entity) {
+                        if(error) {
+                            callback(error)
+                        } else {
+                            entity.status = entity.properties.staatus.values ? entity.properties.staatus.values[0].db_value : 'archive'
+                            if(!$routeParams.filter || $routeParams.filter === entity.status) $scope.lendings.push(entity)
+
+                            callback()
+                        }
+                    })
+                }, callback)
+            }
+        ], function(error) {
+            if(error) {
+                cl(error)
+            }
+            $scope.loading = false
+        })
+    }])
