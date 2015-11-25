8091543795ce56e2580eb26e6fcebced191f1be8
Argo Roots <argo@roots.ee>
2015-11-25 06:41:15 +0200

    Lending view

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
         jade: {
             html: {
                 options: {
-                    pretty: true,
+                    pretty: false,
                 },
                 files: [{
                     expand: true,
