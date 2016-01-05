7e756baf654e8c92132d87c58d7d15af54807be5
Argo Roots <argo@roots.ee>
2016-01-05 16:50:03 +0200

    Entu code cleanup

--- javascripts/entu.js
+++ javascripts/entu.js
@@ -7,11 +7,9 @@ var entu = {}
 entu.getAuthUrl = function(state, redirect_url, http, callback) {
     http.post(entuAPI + 'user/auth', {state: state, redirect_url: redirect_url})
         .success(function(data) {
-            if(data.result) {
-                callback(null, data.result)
-            } else {
-                callback(data)
-            }
+            if(!data.result) { return callback(data) }
+
+            callback(null, data.result)
         })
         .error(function(error) {
             callback(error)
@@ -28,17 +26,15 @@ entu.getUser = function(userId, userToken, http, callback) {
             }
         })
         .success(function(data) {
-            if(data.result) {
-                var user = {
-                    id: data.result.id,
-                    token: data.result.session_key,
-                    name: data.result.name
-                }
+            if(!data.result) { return callback(data) }
 
-                callback(null, user)
-            } else {
-                callback(data)
+            var user = {
+                id: data.result.id,
+                token: data.result.session_key,
+                name: data.result.name
             }
+
+            callback(null, user)
         })
         .error(function(error) {
             callback(error)
@@ -56,11 +52,10 @@ entu.getEntities = function(params, userId, userToken, http, callback) {
             params: params
         })
         .success(function(data) {
-            if(data.result !== []) {
-                callback(null, data.result)
-            } else {
-                callback(data)
-            }
+            if(!data.result) { return callback(data) }
+            if(data.result.length === 0) { return callback(data) }
+
+            callback(null, data.result)
         })
         .error(function(error) {
             callback(error)
@@ -77,22 +72,20 @@ entu.getEntity = function(entityId, userId, userToken, http, callback) {
             }
         })
         .success(function(data) {
-            if(data.result) {
-                var entity = {
-                    _id: data.result.id,
-                    _name: data.result.displayname,
-                    _info: data.result.displayinfo
-                }
-                for(var p in data.result.properties) {
-                    if(!data.result.properties.hasOwnProperty(p)) { continue }
-                    if(!data.result.properties[p].values) { continue }
-                    entity[p] = data.result.properties[p].values[0]
-                }
+            if(!data.result) { return callback(data) }
 
-                callback(null, entity)
-            } else {
-                callback(data)
+            var entity = {
+                _id: data.result.id,
+                _name: data.result.displayname,
+                _info: data.result.displayinfo
             }
+            for(var p in data.result.properties) {
+                if(!data.result.properties.hasOwnProperty(p)) { continue }
+                if(!data.result.properties[p].values) { continue }
+                entity[p] = data.result.properties[p].values[0]
+            }
+
+            callback(null, entity)
         })
         .error(function(error) {
             callback(error)
@@ -109,19 +102,18 @@ entu.getChilds = function(entityId, userId, userToken, http, callback) {
             }
         })
         .success(function(data) {
-            if(data.result) {
-                var entities = []
-                for(var i in data.result) {
-                    if(!data.result.hasOwnProperty(i)) { continue }
-                    for(var n in data.result[i].entities) {
-                        if(!data.result[i].entities.hasOwnProperty(n)) { continue }
-                        entities.push(data.result[i].entities[n])
-                    }
+            if(!data.result) { return callback(data) }
+
+            var entities = []
+            for(var i in data.result) {
+                if(!data.result.hasOwnProperty(i)) { continue }
+                for(var n in data.result[i].entities) {
+                    if(!data.result[i].entities.hasOwnProperty(n)) { continue }
+                    entities.push(data.result[i].entities[n])
                 }
-                callback(null, entities)
-            } else {
-                callback(data)
             }
+
+            callback(null, entities)
         })
         .error(function(error) {
             callback(error)
@@ -138,11 +130,9 @@ entu.getErply = function(method, params, userId, userToken, http, callback) {
             }
         })
         .success(function(data) {
-            if(data.result) {
-                callback(null, data.result)
-            } else {
-                callback(data)
-            }
+            if(!data.result) { return callback(data) }
+
+            callback(null, data.result)
         })
         .error(function(error) {
             callback(error)
@@ -159,11 +149,9 @@ entu.addEntity = function(parentEntityId, properties, userId, userToken, http, c
             }
         })
         .success(function(data) {
-            if(data.result) {
-                callback(null, data.result)
-            } else {
-                callback(data)
-            }
+            if(!data.result) { return callback(data) }
+
+            callback(null, data.result)
         })
         .error(function(error) {
             callback(error)
@@ -180,37 +168,11 @@ entu.changeEntity = function(entityId, properties, userId, userToken, http, call
             }
         })
         .success(function(data) {
-            if(data.result) {
-                callback(null, data.result)
-            } else {
-                callback(data)
-            }
+            if(!data.result) { return callback(data) }
+
+            callback(null, data.result)
         })
         .error(function(error) {
             callback(error)
         })
 }
-
-
-
-// $http.get(erplyAPI + 'saveSalesDocument', {
-//         headers: {
-//             'X-Auth-UserId': $window.sessionStorage.getItem('userId'),
-//             'X-Auth-Token': $window.sessionStorage.getItem('userToken')
-//         },
-//         params: {
-//             type: 'CASHINVOICE',
-//             pointOfSaleID: 2,
-//             customerID: 496,
-//             confirmInvoice: 0,
-//             productID1: 19,
-//             amount1: 2,
-//             productID2: 20,
-//             amount2: 1,
-//             productID3: 21,
-//             amount3: 4,
-//         }
-//     })
-//     .success(function(data) {
-//         cl(data)
-//     })
